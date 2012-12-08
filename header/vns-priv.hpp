61ad8467f00596d60119b8fb842fdcb4c6830c1e
moritz9422 <moritz9422@gmail.com>
2012-12-08 13:08:38 +0100

    Added definition of changeNode-Unit

--- header/vns-priv.hpp
+++ header/vns-priv.hpp
@@ -32,6 +32,19 @@ namespace pcp {
 											  int numSteps);
 	};
 	
+	class changeNode : public VNS_Unit {
+		public:
+			/// Returns a given name for the neighborhood
+			virtual const char *name();
+			
+			/// Compute the new improved solution of this neighborhood
+			virtual Solution *findLocalMin(Solution& curBest, Solution& full);
+			
+			/// Shuffle a solution using the neighborhood as a base
+			virtual Solution *shuffleSolution(Solution& cur, Solution& full,
+											  int numSteps);
+	};
+	
 	/// Implement a dummy tabu search, see VNS_Unit
 	class tabuSearch : public VNS_Unit {
 		public:
