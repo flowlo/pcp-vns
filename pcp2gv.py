452bec03bed0ec19660d91cb884a64c6c017c677
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-01-08 22:59:27 +0100

    Improved pcp2gv.py

--- pcp2gv.py
+++ pcp2gv.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
-vertices, edges, partitions = raw_input().split(' ');
+vertices, edges, partitions = map(int, raw_input().split(' '));
 
 clusters = [];
 
-for i in range(0, int(partitions)):
+for i in range(0, partitions):
 	clusters.append('');
 
 print 'graph pcp {'
 print '\tnode [shape = circle]'
 print '\tstyle = rounded'
 
-for v in range(0, int(vertices)):
+for v in range(0, vertices):
 	clusters[int(raw_input())] += '\t\t' + str(v) + ';\n'
 
 i = 0
@@ -22,7 +22,7 @@ for cluster in clusters:
 	print '\t}'
 	i += 1
 
-for i in range(0, int(edges)):
+for i in range(0, edges):
 	print '\t' + ' -- '.join(raw_input().split(' ')) + ';'
 
 print '}'
