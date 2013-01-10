43dec7e44352c365fc32f1414954172248dd3206
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-01-10 15:53:15 +0100

    Added Solution back to the tree

---
+++ header/Solution.hpp
@@ -0,0 +1,9 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
+#include <iostream>
+#include <string>
+#include <boost/tokenizer.hpp>
+#include <boost/lexical_cast.hpp>
+#include "pcp.hpp"
+#define PARSER_COL_BUFFERSIZE 128
+#endif
