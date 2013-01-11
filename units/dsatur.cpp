332352023693fc240d8e6bb68a6e39c8e1209653
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-01-11 19:32:27 +0100

    Added DSATUR shuffle

--- units/dsatur.cpp
+++ units/dsatur.cpp
@@ -73,5 +73,57 @@ Solution* dsatur::findLocalMin(Solution& curBest, Solution& full) {
 
 /// Shuffle a solution using the neighborhood as a base
 Solution* dsatur::shuffleSolution(Solution& cur, Solution& full, int numSteps) {
-	return new Solution(&cur);
+	Solution* s = &cur;
+	
+	fill(s->partition, s->partition + s->numParts, -1);
+	
+	int numColors = -1, target, maxColorDegree, maxBlankDegree, colorDegree, blankDegree, color;
+	
+	pair<VertexIter, VertexIter> v;
+	
+	for (int j = 0; j < s->numParts; j++) {
+		if (DEBUG_LEVEL == 4)
+			cout << "New iteration!" << endl;
+
+		target = maxColorDegree = maxBlankDegree = -1;
+		
+	 	for (v = vertices(*s->g); v.first != v.second; ++v.first) {
+	 		if (s->isPartitionColored(*v.first)) {
+	 			if (DEBUG_LEVEL == 4)
+		 			cout << "Skipping " << *v.first << endl;
+	 			continue;
+	 		}
+	 		
+			colorDegree = s->getColorDegree(*v.first);
+			
+			if (rand() % 2)
+				if (colorDegree < maxColorDegree)
+					continue;
+			
+			blankDegree = degree(*v.first, *s->g) - colorDegree;
+			
+			if (rand() % 3)
+				if (colorDegree == maxColorDegree && blankDegree <= maxBlankDegree)
+					continue;
+				
+			target = *v.first;
+			maxColorDegree = colorDegree;
+			maxBlankDegree = blankDegree;
+			
+			if (DEBUG_LEVEL == 4)
+				cout << "New best " << target << " having (" << maxColorDegree << "|" << maxBlankDegree << ")" << endl;
+		}
+		
+		color = s->minPossibleColor(target);
+		s->setPartitionColor(target, color);
+		
+		if (DEBUG_LEVEL == 4)
+			cout << "Colored " << target << " with " << color << endl;
+		
+		if (numColors < color)
+			numColors = color;
+	}
+
+	s->colorsUsed = numColors + 1;
+	return s;
 }
