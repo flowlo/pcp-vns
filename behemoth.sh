6a43e65650f44a83c80b2350e1e02022e9536a26
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-02-21 13:07:28 +0100

    Fix behmoth stuff

--- behemoth.sh
+++ behemoth.sh
@@ -24,4 +24,4 @@ esac
 echo Syncing $remote@$host ...
 
 rsync -aiuz -e ssh pcp $remote@$host:$target/pcp-vns+$(git rev-parse --short HEAD)
-ssh $remote@$host git --git-dir=$target checkout behemoth
+ssh $remote@$host git --git-dir=/home/$remote/pcp-vns pull origin behemoth ; git --git-dir=/home/$remote/pcp-vns checkout behemoth
