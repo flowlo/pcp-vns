8395789465c6e6d57e558682167f234c15e6d96b
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-02-26 19:26:54 +0100

    Add demo script for easy demoing and testing

---
+++ demo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+width=$(expr $(xdpyinfo | grep dimensions | tr -s ' ' | cut -d ' ' -f 3 | cut -d 'x' -f 1) / 2)
+height=$(expr $(xdpyinfo | grep dimensions | tr -s ' ' | cut -d ' ' -f 3 | cut -d 'x' -f 2) - 40)
+killall ubigraph
+ubigraph &
+
+echo Waiting for Ubigraph server to settle
+sleep 1
+
+wmctrl -r terminal -e 0,0,0,$width,$height
+wmctrl -r ubigraph -e 0,$width,0,$width,$height
+./pcp -n d -p demo.gv < instances/Table2_random_instances/n20p5t2s3.pcp
+fdp -Tpdf demo.gv > demo.pdf
+evince demo.pdf &
