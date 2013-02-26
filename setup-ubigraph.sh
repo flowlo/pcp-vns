c4c9e6ff379c6666df76f45b2c70c754f28e167a
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-02-26 13:21:59 +0100

    Fix travis again

--- setup-ubigraph.sh
+++ setup-ubigraph.sh
@@ -7,4 +7,4 @@ mv include/UbigraphAPI.h /usr/include
 mv bin/ubigraph_server /usr/bin
 mv examples/C/libubigraphclient.a /usr/lib
 cd ..
-rm UbiGraph-alpha-0.2.4-Linux64-Ubuntu-8.04
+rm -rf UbiGraph-alpha-0.2.4-Linux64-Ubuntu-8.04
