1ecaeee92241d4486cf53aa13776777b0542b13b
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-02-26 18:01:41 +0100

    Add condition for ubigraph header inclusion

--- include/Solution.hpp
+++ include/Solution.hpp
@@ -8,8 +8,10 @@
 #include <boost/tokenizer.hpp>
 #include <boost/lexical_cast.hpp>
 #include "pcp.hpp"
+#ifdef ubigraph
 extern "C"{
 	#include <UbigraphAPI.h>
 }
+#endif
 #define PARSE_BUFFERSIZE 128
 #endif
