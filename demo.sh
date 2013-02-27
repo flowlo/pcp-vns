914d631034130694acd21b29a24a5ed8dd88cf73
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-02-27 12:56:39 +0100

    Refine demo script

--- demo.sh
+++ demo.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
-width=$(expr $(xdpyinfo | grep dimensions | tr -s ' ' | cut -d ' ' -f 3 | cut -d 'x' -f 1) / 2)
-height=$(expr $(xdpyinfo | grep dimensions | tr -s ' ' | cut -d ' ' -f 3 | cut -d 'x' -f 2) - 40)
-killall ubigraph
+resolution=$(xdpyinfo | grep dimensions | tr -s ' ' | cut -d ' ' -f 3)
+width=$(expr $(echo $resolution | cut -d 'x' -f 1) / 2)
+height=$(expr $(echo $resolution | cut -d 'x' -f 2) - 50)
+
+echo 'Starting Ubigraph server'
 ubigraph 2> /dev/null &
+pid=$!
 
-echo Waiting for Ubigraph server to settle
-sleep 1
+echo 'Waiting for Ubigraph server to settle'
+sleep 0.025
 
+echo 'Establishing layout'
 wmctrl -r terminal -e 0,0,0,$width,$height
 wmctrl -r ubigraph -e 0,$width,0,$width,$height
-./pcp -d 4 -n n -p demo.gv < instances/Table2_random_instances/n20p5t2s5.pcp
-fdp -Tpdf demo.gv > demo.pdf
-evince demo.pdf &
+
+echo 'Running'
+./pcp -d 4 -n cnd -p /tmp/demo.gv < instances/Table2_random_instances/n20p5t2s5.pcp
+
+echo 'Rendering printed graph'
+fdp -Tpdf /tmp/demo.gv > /tmp/demo.pdf
+
+echo 'Viewing printed graph'
+evince /tmp/demo.pdf
+
+echo 'Cleaning temporary files'
+rm /tmp/demo.gv /tmp/demo.pdf
+
+echo 'Killing Ubigraph server'
+kill $pid
