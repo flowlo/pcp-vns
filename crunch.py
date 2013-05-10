dde7e0b79462a10599212e9239cc637e8ad4cbf9
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2013-05-10 17:41:05 +0200

    Copy permute to scripts, remove permute.py
    Including permute.py made the cluster nodes cry, so I decided to
    go one step back and duplicate it again.
    
    Also fixed a small path bug in crunch.py.

--- crunch.py
+++ crunch.py
@@ -3,9 +3,19 @@
 import glob, sys, os, permute
 from os import mkdir
 from os.path import abspath, basename
-from permute import permute
 from subprocess import call
 from sys import argv, exit
+
+def permute (word):
+	result = []
+	if len(word) == 1:
+		result.append(word)
+	else:
+		for pos in range(len(word)):
+			permutations = permute(word[0:pos] + word[pos + 1:len(word)])
+			for item in permutations:
+				result.append(word[pos] + item)
+	return result
 
 args = '-s 10 -i 20 -u 1000 -t 600'
 
@@ -18,7 +28,7 @@ permutations = permute(argv[2])
 output = abspath(argv[3]) + '/'
 
 for i in range(runs):
-	mkdir(output + '/' + str(i))
+	mkdir(output + str(i))
 	for n in argv[4:]:
 		for p in permutations:
 			base = output + '/' + str(i) + '/' + basename(n) + '.' + p
