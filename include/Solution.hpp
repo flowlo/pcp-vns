0a1acf89941c2ab70f6c19a713f731a9a60049df
moritz9422 <moritz9422@gmail.com>
2014-01-27 22:06:35 +0100

    First stab with new directory structure and new Solution implementation
    Build new directory tree for later use of autoconf and automake
    First draft of Solution.hpp
    Implemented first draft of class Solution

---
+++ include/Solution.hpp
@@ -0,0 +1,116 @@
+#ifndef _SOLUTION_HPP_
+#define _SOLUTION_HPP_
+
+#include <cstdint>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/filtered_graph.hpp>
+#include <boost/shared_array.hpp>
+#include <memory>
+
+namespace pcp {
+	typedef std::uint64_t vertex_t;
+	typedef std::uint64_t edge_t;
+	typedef std::uint64_t partition_t;
+	typedef std::int64_t color_t;
+	
+	// Stores the partition info for each vertex
+	typedef boost::property<boost::vertex_index1_t, partition_t> VertexProperty;
+	
+	/* TODO: Is this really necessary?
+	// Properties that are stored for each edge in the graph
+  	typedef boost::property<boost::edge_index_t, int> EdgeProperty; */
+  	
+  	// The unfiltered full graph type
+  	typedef boost::adjacency_list<boost::vecS, boost::vecS,
+                boost::undirectedS, VertexProperty> Graph;
+	
+	// Predicate to select visible edges
+	struct edge_visible {
+		edge_visible() { }
+		edge_visible(boost::shared_array<bool> m_v,
+						 std::shared_ptr<Graph> graph) {
+			mapped_vertices = m_v;
+			g = graph;
+		}
+		template <typename Ed> bool operator()(const Ed& e) const {
+			return mapped_vertices[source(e, *g)] && mapped_vertices[target(e, *g)];
+		}
+	  	boost::shared_array<bool> mapped_vertices;
+	  	std::shared_ptr<Graph> g;
+	};
+	
+	// Predicate to select visible vertices
+	struct vertex_visible {
+		vertex_visible() { }
+		vertex_visible(boost::shared_array<bool> m_v) {
+			mapped_vertices = m_v;
+		}
+		template <typename Ve> bool operator()(const Ve& v) const {
+			return mapped_vertices[v];
+		}
+	  	boost::shared_array<bool> mapped_vertices;
+	};
+   
+   // The filtered graph, only showing visible edges and vertices
+   typedef boost::filtered_graph<Graph, edge_visible, 
+   										vertex_visible> FilterGraph;
+   										
+ 	// Represents a vertex
+	typedef boost::graph_traits<FilterGraph>::vertex_descriptor Vertex;
+
+	// Represents an edge
+	typedef boost::graph_traits<FilterGraph>::edge_descriptor Edge;
+
+	// Standard vertex_iterator
+	typedef boost::graph_traits<FilterGraph>::vertex_iterator VertexIter;
+
+	// Standard vertex_iterator
+	typedef boost::graph_traits<FilterGraph>::edge_iterator EdgeIter;
+
+	// Iterator for adjacent vertices
+	typedef boost::graph_traits<FilterGraph>::adjacency_iterator AdjIter;
+
+	// Stores one Solution, using a filtered graph
+	// Provides methods to manipulating the stored Solution
+	class Solution {
+		public:
+			Solution(std::uint32_t num_vertices, std::uint32_t num_partition);
+			Solution(const Solution& copy);
+			~Solution();
+		
+			Solution& operator=(const Solution& rhs);
+			
+			partition_t getPartition(const Vertex& v);
+			color_t getColor(const Vertex& v);
+			std::uint32_t getColorDegree(const Vertex& v);
+			color_t minPossibleColor(const Vertex& v);
+			std::pair<std::uint32_t, color_t> 
+						getColorDegreeAndMinColor(const Vertex& v);
+			const FilterGraph& getCurrentSolution();
+			const Graph& getFullGraph();
+			
+			bool isColored(const Vertex& v);
+			
+			void setVertexColor(const Vertex& v, color_t color);
+			
+			void setPartition(const Vertex& v, partition_t part);
+			void addEdge(const Vertex& v1, const Vertex& v2);
+			
+			void toggleVertex(const Vertex& v);
+			void replaceVertex(const Vertex& toRep, const Vertex& nv);
+			void detach();
+		private:
+			std::shared_ptr<Graph> g;
+			std::shared_ptr<FilterGraph> fg;
+			boost::shared_array<bool> mapped_vertices;
+			boost::shared_array<std::vector<Vertex>> part_vertices;
+			color_t* coloring;
+			
+			std::int32_t colors_used;
+			std::uint32_t num_parts;
+			std::uint32_t num_vertices;
+			std::uint32_t num_edges;
+	};
+}
+
+#endif
---
+++ src/Solution.cpp
@@ -0,0 +1,228 @@
+#include "../include/Solution.hpp"
+
+#include <cstdint>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/filtered_graph.hpp>
+#include <boost/shared_array.hpp>
+#include <memory>
+#include <algorithm>
+
+using namespace std;
+
+namespace pcp {
+	Solution :: Solution(uint32_t num_vertices, uint32_t num_partition) {
+	
+		shared_ptr<Graph> full(new Graph(num_vertices));
+		boost::shared_array<bool> mapped(new bool[num_vertices]);
+		// Construct the predicates for the filtered graph based on the bool array
+		edge_visible edges(mapped, full);
+		vertex_visible vertices(mapped);
+		shared_ptr<FilterGraph> fg(new FilterGraph(*full, edges, vertices));
+		this->fg = fg;
+		this->mapped_vertices = mapped;
+		
+		boost::shared_array<vector<Vertex>> vparts(
+															new vector<Vertex>[num_partition]);
+		this->part_vertices = vparts;
+		
+		this->coloring = new color_t[num_partition];
+		
+		this->g = full;
+		this->num_parts = num_partition;
+		this->num_vertices = num_vertices;
+		this->num_edges = 0;
+		this->colors_used = -1;
+		
+		fill_n(mapped.get(), num_vertices, true);
+		fill_n(coloring, num_partition, -1);
+	}
+	
+	Solution :: Solution(const Solution& cp) {
+		// Copy all const values
+		this->g = cp.g;
+		this->fg = cp.fg;
+		this->num_parts = cp.num_parts;
+		this->num_edges = cp.num_edges;
+		this->num_vertices = cp.num_vertices;
+		this->colors_used = cp.colors_used;
+		this->part_vertices = cp.part_vertices;
+		
+		// Add new coloring array, and copy the old values
+		this->coloring = new color_t[this->num_parts];
+		copy(cp.coloring, cp.coloring + num_parts, this->coloring);
+		
+		// Use old mapping array, add one manually later
+		boost::shared_array<bool> mapped(cp.mapped_vertices);
+		this->mapped_vertices = mapped;
+	}
+	
+	Solution :: ~Solution() {
+		// Deallocate color array, smart pointer do the rest
+		delete[] coloring;
+	}
+
+	Solution& Solution :: operator=(const Solution& rhs) {
+		// Check for weird assignments
+		if (this == &rhs)
+			return *this;
+			
+		// Delete old array, smart pointers can do the rest for themselves
+		delete[] coloring;
+
+		// Copy const values
+		this->g = rhs.g;
+		this->fg = rhs.fg;
+		this->num_parts = rhs.num_parts;
+		this->num_edges = rhs.num_edges;
+		this->num_vertices = rhs.num_vertices;
+		this->colors_used = rhs.colors_used;
+		this->part_vertices = rhs.part_vertices;
+		
+		// Add new coloring array and copy values from rhs
+		this->coloring = new color_t[this->num_parts];
+		copy(rhs.coloring, rhs.coloring + num_parts, this->coloring);
+		
+		// Use old mapping array, add one manually later
+		boost::shared_array<bool> mapped(rhs.mapped_vertices);
+		this->mapped_vertices = mapped;
+		return *this;
+	}
+	
+	// Straightforward access to the property map, return partitionID
+	partition_t Solution :: getPartition(const Vertex& v) {
+		return get(boost::vertex_index1_t(), *this->fg, v);
+	}
+	
+	// Straightforward access to color of the vertex
+	color_t Solution :: getColor(const Vertex& v) { 
+		return coloring[getPartition(v)];
+	}
+	
+	// Step through all adjacent vertices !IN THE FILTERED GRAPH! and count the 
+	// number of colored vertices
+	uint32_t Solution :: getColorDegree(const Vertex& v) {
+		uint32_t colored = 0;
+		AdjIter i, end;
+		
+		for (tie(i, end) = adjacent_vertices(v, *this->fg); i != end; i++) 
+			if (this->isColored(*i))
+				colored++;
+		
+		return colored;
+	}
+	
+	// Step through all adjacent vertices !IN THE FILTERED GRAPH! and check for
+	// the min color possible
+	color_t Solution :: minPossibleColor(const Vertex& v) {
+		bool* colors = new bool[this->num_parts];
+		fill_n(colors, this->num_parts, false);
+		AdjIter i, end;
+
+		color_t c;
+		// Mark all colors which are used by neighbors of node
+		for (tie(i, end) = adjacent_vertices(v, *this->fg); i != end; i++)
+			if ((c = getColor(*i)) >= 0)
+				colors[c] = true;
+
+		// find the first unused color
+		for (int i = 0; i < this->num_parts; i++) {
+			if (!colors[i]) {
+					delete[] colors;
+					return i;
+			}
+		}
+
+		// This cant happen, but maybe it will...
+		delete[] colors;
+		return -1;
+	}
+	
+	// Basically a combination of minPossibleColor() and getColorDegree() to 
+	// speed things up
+	pair<uint32_t, color_t> Solution :: getColorDegreeAndMinColor
+																			(const Vertex& v) {
+		bool* colors = new bool[this->num_parts];
+		color_t c;
+		uint32_t colored = 0;
+		pair<uint32_t, color_t> ret;
+		fill_n(colors, this->num_parts, false);
+		AdjIter i, end;
+
+		// Mark all colors which are used by neighbors of node
+		for (tie(i, end) = adjacent_vertices(v, *this->fg); i != end; i++) {
+			if ((c = getColor(*i)) >= 0) {
+				colors[c] = true;
+				colored++;
+			}
+		}
+
+		// find the first unused color
+		for (color_t i = 0; i < this->num_parts; i++) {
+			if (!colors[i]) {
+					delete[] colors;
+					ret.first = colored;
+					ret.second = i;
+					return ret;
+			}
+		}
+		
+		// This cant happen, but maybe it will...
+		ret.first = 0;
+		ret.second = -1;
+		delete[] colors;
+		return ret;
+	}
+
+	// Check wheter a vertex is colored or not
+	bool Solution :: isColored(const Vertex& v) {
+		return getColor(v) > -1;
+	}
+
+	// Set a specific vertex to a specific color
+	void Solution :: setVertexColor(const Vertex& v, color_t color) {
+		this->coloring[getPartition(v)] = color;
+	}
+
+	// Set an previously unset vertices to a specific partition and 
+	// push it in the correspondig vector for easy access
+	void Solution :: setPartition(const Vertex& v, partition_t part) {
+		put(boost::vertex_index1_t(), *this->g, v, part);
+		this->part_vertices[part].push_back(v);
+	}
+	
+	// Add an edge to the graph, visibility depends on the vertices
+	void Solution :: addEdge(const Vertex& v1, const Vertex& v2) {
+		add_edge(v1, v2, *this->g);
+	}
+
+	// Toggle the visibility of a vertex in the filtered graph
+	void Solution :: toggleVertex(const Vertex& v) {
+		this->detach();
+		this->mapped_vertices[v] = !this->mapped_vertices[v];
+	}
+
+	// Replace on vertex with the other, done through toggeling visibilty	
+	void Solution :: replaceVertex(const Vertex& toRep, const Vertex& nv) {
+		this->toggleVertex(toRep);
+		this->toggleVertex(nv);
+	}
+	
+	// Detach the current maping from the others
+	void Solution :: detach() {
+		if (this->mapped_vertices.use_count() > 1) {
+			boost::shared_array<bool> mapped(new bool[this->num_vertices]);
+			copy(this->mapped_vertices.get(), 
+				  this->mapped_vertices.get() + this->num_vertices,
+				  mapped.get());
+			this->mapped_vertices = mapped;
+		}
+	}
+	
+	const FilterGraph& Solution :: getCurrentSolution() {
+		return *this->fg;
+	}
+	
+	const Graph& Solution :: getFullGraph() {
+		return *this->g;
+	}
+}
