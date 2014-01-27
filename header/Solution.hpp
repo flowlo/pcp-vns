193b2b407df65ede589079421254142197265f7d
moritz9422 <moritz9422@gmail.com>
2014-01-27 14:33:35 +0100

    Making a clean slate for a new version of vns-pcp
    Deleted all old source files except for handy tools for working with pcp-files

--- header/Solution.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SOLUTION_H
-#define SOLUTION_H
-#include <iostream>
-#include <string>
-#include <boost/tokenizer.hpp>
-#include <boost/lexical_cast.hpp>
-#include "pcp.hpp"
-#define PARSE_BUFFERSIZE 128
-#endif
