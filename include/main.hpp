2262c46b4f532a855947618621117c28dc3a0888
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2014-02-18 19:43:20 +0100

    Bring back main.hpp

---
+++ include/main.hpp
@@ -0,0 +1,14 @@
+// Contains all necessary includes for the main method of the pcp-program
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>                  // for //cout
+#include <fstream>
+#include <vector>
+#include <cstring>
+#include <utility>                   // for pair
+#include <boost/program_options.hpp>
+#include "pcp.hpp"
+#include "oneStepCd.hpp"
+#include "vns.hpp"
+#endif
