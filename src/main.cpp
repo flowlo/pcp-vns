d11913fa4136b1155e6b741f9a0b208e068849b1
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2014-02-18 16:51:19 +0100

    Pull main and VNS from master, small changes towards ng

---
+++ src/main.cpp
@@ -0,0 +1,166 @@
+#include "../include/main.hpp"
+#include <boost/algorithm/string/predicate.hpp>
+using namespace std;
+using namespace pcp;
+using namespace boost;
+using namespace boost::program_options;
+using namespace boost::algorithm;
+
+int DEBUG_LEVEL = 2;
+
+// main method, starting point of the program
+int main(int argc, char* argv[]) {
+	string units, printFile, inputFile;
+	int unsuccessfulShake, shakeStart, shakeIncrement, maxTime, rSeed;
+	
+	// Add program options to be parsed using boost:program_options
+	options_description options("General options");
+	options.add_options()
+		("help,h", "produce help message")
+		("print,p", value<string>(&printFile), "set print file (*.gv)")
+		("debug,d", value<int>(&DEBUG_LEVEL)->default_value(0), "set debug level")
+	;
+	
+	options_description vnsOptions("Variable Neighborhood Search options");
+	vnsOptions.add_options()
+		("units,n", value<string>(&units)->default_value("nc"), "set units")
+		("shakeStart,s", value<int>(&shakeStart)->default_value(0), "set shake start")
+		("shakeIncrement,i", value<int>(&shakeIncrement)->default_value(10), "set shake increment")
+		("unsuccessfulShake,u", value<int>(&unsuccessfulShake)->default_value(10), "set unsuccessful shake threshold")
+		("maxTime,t", value<int>(&maxTime)->default_value(10), "set VNS running time (seconds)")
+		("checkFinal,c", value<bool>()->zero_tokens() , "disable final check after VNS has finished")
+		("checkIntermediate,m", value<bool>()->zero_tokens(), "enable check after each improvement/shake")
+		("seed,r", value<int>(&rSeed)->default_value(time(NULL)), "set seed for random number generator")
+		#ifdef ubigraph
+		("delay", value<bool>()->zero_tokens(), "delay execution after loading the graph and finishing onestepCD")
+		("full", value<bool>()->zero_tokens(), "render the full graph after the VNS terminates")
+		#endif
+	;
+	
+	// Parse the options here
+	options_description all("Allowed options");
+	all.add(options).add(vnsOptions);
+	
+	variables_map vm;
+	try {
+		store(parse_command_line(argc, argv, all), vm);
+	}
+	catch(const boost::program_options::error& ex) {
+		cerr << ex.what() << endl;
+		return -1;
+	}
+	notify(vm);
+	
+	if (vm.count("help")) {
+		cout << all << endl;
+		return 0;
+	}
+	
+	// Set random number generator's seed here, normally time related, unless
+	// specified otherwise
+	srand(rSeed);
+	
+	if (DEBUG_LEVEL > 4) {
+		DEBUG_LEVEL = 4;
+		
+		cout << "The specified debug level (-d) exceeds"
+		<< " the maximum. It was set to " << DEBUG_LEVEL
+		<< "." << endl;
+	}
+	else if (DEBUG_LEVEL < 0) {
+		DEBUG_LEVEL = 0;
+	}
+
+	// New solution for the full graph
+	Solution fullG;
+
+	// Decide which format the input is in, and parse accordingly
+	if (isdigit(cin.peek())) {
+		string buffer = "";
+		getline(cin, buffer);
+		cin.seekg(0);
+		
+		/*if (buffer.find(' ') == string::npos) {
+			if (DEBUG_LEVEL > 2)
+				cout << "Detected .col.b input." << endl;
+			fullG = Solution::fromColBStream(cin);
+		}
+		else {*/
+			if (DEBUG_LEVEL > 2)
+				cout << "Detected .pcp input." << endl;
+			fullG = readPcpStream(cin);
+		//}
+	}
+	/*else {
+		if (DEBUG_LEVEL > 2)
+			cout << "Detected .col input." << endl;
+	
+		fullG = Solution::fromColStream(cin);
+	}*/
+	
+	if (fullG == NULL) {
+		cerr << "Failed to parse input!" << endl;
+		return -1;
+	}
+
+	// In case of ubigraph visualization stop the program for 3 secs
+	#ifdef ubigraph
+	if (vm.count("delay"))
+		usleep(3000000);
+	#endif
+
+	if (DEBUG_LEVEL > 3) {
+		cout << "Constructing initial solution ..." << endl;
+	}
+	
+	// Calcutalte initial solution using oneStepCd
+	Solution onestep = onestepCD(fullG);
+	
+	if (DEBUG_LEVEL > 2)
+		cout << "Initial solution uses " << onestep->colorsUsed << " colors." << endl;
+
+	// In case of ubigraph visualization stop here for 3 secs
+	#ifdef ubigraph
+	Solution *onecopy = new Solution(onestep);
+
+	if (vm.count("delay"))
+		usleep(3000000);
+	#endif
+
+	// Start Vns
+	Solution *best = vnsRun(*onestep, *fullG, units, unsuccessfulShake, shakeStart, shakeIncrement, maxTime, vm.count("checkIntermediate"), !vm.count("checkFinal"));
+	
+	if (best == NULL)
+		return -1;
+	
+	// print output if option is set
+	if (vm.count("print")) {
+		ofstream out(vm["print"].as<string>());
+		
+		if (out.fail()) {
+			cerr << "Error when trying to access file: " << vm["print"].as<string>() << endl;
+			return -1;
+		}
+		best->print(out);
+		out.flush();
+		out.close();
+		
+		if (DEBUG_LEVEL > 1)
+			cout << "Printing to '" << vm["print"].as<string>() << "' done!" << endl;
+	}
+	
+	// Visualize the existing onesteo solution along side the new best solution
+	#ifdef ubigraph
+	int offset = num_vertices(*fullG->g);
+	onecopy->redraw(offset);
+	if (vm.count("full"))
+		fullG->redraw(offset * 2);
+	delete onecopy;
+	#endif
+	
+	// Clean up duty
+	delete best;
+	delete fullG;
+
+	return 0;
+}
