91374c3a81d0f01f9a829ceae77befde95f1f166
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2014-02-18 19:32:21 +0100

    Use C++ header naming scheme

---
+++ include/vns.hpp
@@ -0,0 +1,19 @@
+/// This file contains all definitions for the Variable Neighborhood Search
+/// for the PC-Problem
+#ifndef VNS_H
+#define VNS_H
+#include "pcp.hpp"
+#include <vector>
+#include <boost/unordered_set.hpp>
+#include <boost/functional/hash.hpp>
+
+namespace pcp {
+	/// Run the VNS with a maximum of k iterations.
+	/// Returns after either there were a number of unsuccessful shakes 
+	/// or if 'time' seconds have elapsed.
+	/// Shaking starts with shakeStart steps and is then incremented by
+	/// shakeIncrement.
+	Solution *vnsRun(Solution& best, Solution& orig, std::string units, int unsuccessfulShake, int shakeStart, int shakeIncrement, int maxTime, bool checkIntermediate, bool checkFinal);
+}
+
+#endif
