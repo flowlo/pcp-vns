ac0d1410e2c1cdca948d00d1f44c985ac413fe02
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2014-02-21 15:06:09 +0100

    Reimplement changeNode

--- src/units/changeNode.cpp
+++ src/units/changeNode.cpp
@@ -12,8 +12,67 @@ const char changeNode::getAbbreviation() {
 	return 'n';
 }
 
-Solution changeNode::findLocalMin(Solution& best) {
-	return best;
+Solution changeNode::findLocalMin(Solution& solution) {
+	int maxColor = solution.getColorsUsed() - 1;
+
+	FVertexIter i, end;
+	vector<Vertex> candidates;
+
+	// Search all vertices for minimal colors
+	for (tie(i, end) = vertices(solution.getCurrentSolution()); i != end; i++) {
+		if (solution.getColor(*i) == maxColor) {
+			// Only execute changeNode if there are nodes to replace
+			candidates = solution.getPartitionNodes(solution.getPartition(*i));
+			if (candidates.size() > 1) {
+				vector<Vertex>::iterator replacement;
+				for (replacement = candidates.begin(); replacement != candidates.end(); replacement++) {
+					if (*replacement != *i) {
+						solution.replaceVertex(*i, *replacement);
+
+						// Check for improvement
+						int color = solution.minPossibleColor(*replacement);
+						if (color < maxColor) {
+							solution.setVertexColor(*replacement, color);
+							break;
+						}
+						else
+							solution.replaceVertex(*replacement, *i);
+					}
+				}
+				// No candidates left to try...
+				if (replacement == candidates.end()) {
+					solution.setColorsUsed(solution.getNumPartition());
+					#ifdef ubigraph
+					solution.redraw();
+					#endif
+					return solution;
+				}
+			}
+			else
+				return solution;
+		}
+	}
+
+	maxColor = 0;
+	for (uint32_t i = 0; i < solution.getNumPartition(); i++)
+		if (solution.getColor(i) > maxColor)
+			maxColor = solution.getColor(i);
+
+	solution.setColorsUsed(maxColor + 1);
+
+	if (DEBUG_LEVEL > 1) {
+		cout<<"changeNode uses "<<solution.getColorsUsed()<<" colors"<<endl; 
+	}
+
+	Solution temp = this->findLocalMin(solution);
+	if (temp < solution) {
+		solution = temp;
+		#ifdef ubigraph
+		solution.redraw();
+		#endif
+	}
+
+	return solution;
 }
 
 Solution changeNode::shuffleSolution(Solution& cur, int numSteps) {
